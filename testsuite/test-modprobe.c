723ad962b4faa41e9d82e17e47b056361b99bd60
Emil Velikov <emil.l.velikov@gmail.com>
2026-07-18 14:14:50 +0100

    testsuite: move modprobe test into test-modprobe.c
    ... alongside all the other modprobe tests.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Link: https://github.com/kmod-project/kmod/pull/450
    Signed-off-by: Lucas De Marchi <ldemarchi@kernel.org>

--- testsuite/test-modprobe.c
+++ testsuite/test-modprobe.c
@@ -147,6 +147,20 @@ DEFINE_TEST(modprobe_weakdep_loop,
 	.modules_not_loaded = "mod-loop-a,mod-simple-c",
 	);
 
+static int modprobe_weakdep_config(void)
+{
+	return EXEC_TOOL(modprobe, "-c");
+}
+DEFINE_TEST(modprobe_weakdep_config,
+	.description = "check modprobe config parsing with weakdep",
+	.config = {
+		[TC_UNAME_R] = "4.4.4",
+		[TC_ROOTFS] = TESTSUITE_ROOTFS "test-modprobe/weakdep-config",
+	},
+	.output = {
+		.out = TESTSUITE_ROOTFS "test-modprobe/weakdep-config/correct.txt",
+	});
+
 static int modprobe_install_cmd_loop(void)
 {
 	return EXEC_TOOL(modprobe, "mod-loop-a");
